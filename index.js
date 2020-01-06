a51d06289eff976f400bdc9f0929a1b9531bd3f3
oom <venom0218@gmail.com>
2020-01-06 15:09:13 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+let REQUEST = require('request');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+var options = { gzip: true, jar: true };
+options.method = "GET";
+options.url = "https://www.google.com";
+REQUEST(options, (err, res, body) => {
+    var { window } = new JSDOM(body);
+    var $ = require('jquery')(window);
+    var links_on_page = $('a');
+});
